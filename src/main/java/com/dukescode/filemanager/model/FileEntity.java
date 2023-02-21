8f6557546d0d3ee964cbb571ba19821f2dc1b1d6
dukefs <dukefs@dappinblocks.com>
2023-02-21 02:19:57 -0300

    Add dependency of javax.persistence and implement FileEntity POJO Class.

--- src/main/java/com/dukescode/filemanager/model/FileEntity.java
+++ src/main/java/com/dukescode/filemanager/model/FileEntity.java
@@ -1,5 +1,34 @@
 package com.dukescode.filemanager.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "files")
+
 public class FileEntity {
-    
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+    private String contentType;
+    private Long size;
+
+    @Lob
+    private byte[] data;
+
 }

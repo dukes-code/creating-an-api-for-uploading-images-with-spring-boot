aaa7b349066756b87e5a5f0a58aa0afcf1f41c44
dukefs <dukefs@dappinblocks.com>
2023-02-22 00:37:03 -0300

    Delete javax from pom.xml. Adjust of annotation inside FileEntity. Add actuator to project. Configuration of database connection and multipart sizes.

--- src/main/java/com/dukescode/filemanager/model/FileEntity.java
+++ src/main/java/com/dukescode/filemanager/model/FileEntity.java
@@ -1,23 +1,23 @@
 package com.dukescode.filemanager.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @Entity
 @Table(name = "files")
-
 public class FileEntity {
 
     @Id
